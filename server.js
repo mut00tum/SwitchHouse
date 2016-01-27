2fc859068e8680527dea36a4308a2124e6006538
mut00tum <mmuuttuu@gmail.com>
2016-01-27 16:37:15 +0900

    test0127-1

--- server.js
+++ server.js
@@ -22,37 +22,37 @@ console.log( "start listening at 8000" );
 app.listen(port);
 
 
-app.get( "/hems" , function (req, res){
-  // console.log( 'GET: /switch' );
-  try {
-    res.set('Content-Type', 'application/json');
-    var data = JSON.parse( fs.readFileSync( Path.json , 'utf8' ) );
-    res.jsonp( data );
-    // console.log( data )
-    res.status(200).end();
-  } catch(e) {
-    console.log( 'GET: /switch: error!' )
-  }
-});
+// app.get( "/hems" , function (req, res){
+//   // console.log( 'GET: /switch' );
+//   try {
+//     res.set('Content-Type', 'application/json');
+//     var data = JSON.parse( fs.readFileSync( Path.json , 'utf8' ) );
+//     res.jsonp( data );
+//     // console.log( data )
+//     res.status(200).end();
+//   } catch(e) {
+//     console.log( 'GET: /switch: error!' )
+//   }
+// });
 
-app.post("/off", function (req, res){
-  try {
-    // console.log( 'POST: /off:' + req.body );
-    fs.writeFileSync( Path.json , JSON.stringify(req.body));
-    res.status(200).end();
-  } catch(e) {
-    console.log( 'POST: /off: error!' )
-  }
-});
+// app.post("/off", function (req, res){
+//   try {
+//     // console.log( 'POST: /off:' + req.body );
+//     fs.writeFileSync( Path.json , JSON.stringify(req.body));
+//     res.status(200).end();
+//   } catch(e) {
+//     console.log( 'POST: /off: error!' )
+//   }
+// });
 
-app.post("/on", function (req, res){
-  try {
-    console.log( 'POST: /on:' + req.body );
-    fs.writeFileSync( Path.json , JSON.stringify(req.body));
-    res.status(200).end();
-  } catch(e) {
-    console.log( 'POST: /on: error!' )
-  }
-});
+// app.post("/on", function (req, res){
+//   try {
+//     console.log( 'POST: /on:' + req.body );
+//     fs.writeFileSync( Path.json , JSON.stringify(req.body));
+//     res.status(200).end();
+//   } catch(e) {
+//     console.log( 'POST: /on: error!' )
+//   }
+// });
 
 
